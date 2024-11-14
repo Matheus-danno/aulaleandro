e9d70f95e37299933045acf381de1d2fc2b47c49
Matheus <matheusdanno@gmail.com>
2024-11-13 21:24:50 -0300

    primeiro projeto

---
+++ Index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Gestão Agronegócio</title>
+    <h1 style="text-align: center;">Sistema de Gestão Agronegócio</h1>
+
+    <link rel="stylesheet" href="CSS/default.css">
+    <link rel="stylesheet" href="CSS/reset.min.css">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/button.css">
+
+
+</head>
+<body>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <a href="cadastro_propriedades/propriedades.php"><button style="display: block; margin: 0 auto;">Gerenciar Propriedades</button></a>
+    <br/>
+    <br/>
+    <a href="cadastro_equipamentos/equipamentos.php"><button style="display: block; margin: 0 auto;">Gerenciar Equipamentos</button></a>
+    <br/>
+    <br/>
+    <a href="cadastro_culturas/culturas.php"><button style="display: block; margin: 0 auto;">Gerenciar Culturas</button></a>
+</body>
+</html>
